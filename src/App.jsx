492050e019f4c0d95fbae2fde1a8a848aa16bae3
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2023-12-18 09:02:03 +0100

    Lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import './App.css'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+// layouts
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+        </Route>
+    )
+)
+// const router = createBrowserRouter(routesFromElements)
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Home />,
+//     },
+//     {
+//         path: '/about',
+//         element: <About />,
+//     },
+// ])
 
 function App() {
-    return <div className="App">Hello Techover</div>
+    return <RouterProvider router={router} />
 }
 
 export default App
